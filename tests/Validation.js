c2a92312913a00a341d96aea6b488dcdf3e75abf
Samy Pesse <samypesse@gmail.com>
2016-08-13 22:45:15 +0200

    Add unit test for maxLength

--- tests/Validation.js
+++ tests/Validation.js
@@ -2,7 +2,6 @@ const expect = require('expect');
 const { Validation } = require('../src');
 
 describe('Validation', function() {
-
     describe('minLength', function() {
         const validate = Validation.minLength(10, 'err message');
 
@@ -15,7 +14,19 @@ describe('Validation', function() {
         it('should pass if length is superior', function() {
             validate('Hello World !!');
         });
-
     });
 
+    describe('maxLength', function() {
+        const validate = Validation.maxLength(10, 'err message');
+
+        it('should fail if length is superior', function() {
+            expect(function () {
+                validate('Hello World !!');
+            }).toThrow('err message');
+        });
+
+        it('should pass if length is inferior', function() {
+            validate('Hello');
+        });
+    });
 });
