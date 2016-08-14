c169b770023f2f083e03e65237c19c9b732a299e
Samy Pesse <samypesse@gmail.com>
2016-08-14 13:51:48 +0200

    Add unit tests for Validation.required

--- tests/Validation.js
+++ tests/Validation.js
@@ -5,7 +5,7 @@ describe('Validation', function() {
     describe('.minLength', function() {
         const validate = Validation.minLength(10, 'err message');
 
-        it('should fail if length is inferior', function() {
+        it('should throw if length is inferior', function() {
             expect(function () {
                 validate('Hello');
             }).toThrow('err message');
@@ -19,7 +19,7 @@ describe('Validation', function() {
     describe('.maxLength', function() {
         const validate = Validation.maxLength(10, 'err message');
 
-        it('should fail if length is superior', function() {
+        it('should throw if length is superior', function() {
             expect(function () {
                 validate('Hello World !!');
             }).toThrow('err message');
@@ -27,6 +27,32 @@ describe('Validation', function() {
 
         it('should pass if length is inferior', function() {
             validate('Hello');
+        });
+    });
+
+    describe('.default', function() {
+        const validate = Validation.default('value');
+
+        it('should return default value when undefined', function() {
+            expect(validate(undefined)).toBe('value');
+        });
+
+        it('should return value if null', function() {
+            expect(validate(null)).toBe(null);
+        });
+    });
+
+    describe('.required', function() {
+        const validate = Validation.required('err message');
+
+        it('should throw if value is undefined', function() {
+            expect(function () {
+                validate(undefined);
+            }).toThrow('err message');
+        });
+
+        it('should pass if value is null', function() {
+            expect(validate(null)).toBe(null);
         });
     });
 });
